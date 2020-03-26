b282ae22c62f6ec0a5b2cffb9b916a07f7d6fd9f
SupDex <symeton@gmail.com>
2020-03-26 20:55:09 +0300

    remove useless variables

--- spec/services/players/exists_player_with_indicator_in_last_matches_service_spec.rb
+++ spec/services/players/exists_player_with_indicator_in_last_matches_service_spec.rb
@@ -9,15 +9,12 @@ describe Players::ExistsPlayerWithIndicatorInLastMatchesService do
   let(:player_four) { create(:player, team: team_one) }
   let(:player_five) { create(:player, team: team_one) }
 
-  let(:match) { create(:match) }
-  let(:match_two) { create(:match) }
-
   describe '.call' do
     before do
-      create_list(:match_player_indicator, 2, match: match, player: player)
-      create_list(:match_player_indicator, 2, match: match, player: player_two)
-      create_list(:match_player_indicator, 2, match: match_two, player: player_three)
-      create_list(:match_player_indicator, 2, match: match_two, player: player_four)
+      create_list(:match_player_indicator, 2, player: player)
+      create_list(:match_player_indicator, 2, player: player_two)
+      create_list(:match_player_indicator, 2, player: player_three)
+      create_list(:match_player_indicator, 2, player: player_four)
       create_list(:match_player_indicator, 2, player: player_five)
       create_list(:match_player_indicator, 10)
     end
@@ -27,7 +24,7 @@ describe Players::ExistsPlayerWithIndicatorInLastMatchesService do
     end
 
     it 'returns true' do
-      create(:match_player_indicator, match: match, indicator: indicator, player: player)
+      create(:match_player_indicator, indicator: indicator, player: player)
       create_list(:match_player_indicator, 2, player: player_five)
 
       expect(player.exists_indicator_in_last_matches?(indicator)).to eq(true)
