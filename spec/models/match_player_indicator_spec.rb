6751c8c242b231dd839ec55a500437db42b26443
SupDex <symeton@gmail.com>
2020-03-27 12:09:32 +0300

    add spec for unique match_player_indicator records

--- spec/models/match_player_indicator_spec.rb
+++ spec/models/match_player_indicator_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe MatchPlayerIndicator, type: :model do
+  let(:player) { create(:player) }
+  let(:match) { create(:match) }
+  let(:indicator) { create(:indicator) }
+  let!(:match_player_indicator) { create(:match_player_indicator, indicator: indicator, match: match, player: player) }
+
   it { is_expected.to belong_to(:match) }
   it { is_expected.to belong_to(:player) }
   it { is_expected.to belong_to(:indicator).optional }
   it { is_expected.to have_db_index(%i[match_id player_id indicator_id]) }
+  it { expect{ described_class.create(match: match, player: player, indicator: indicator) }.to raise_error(ActiveRecord::RecordNotUnique) }
 end
