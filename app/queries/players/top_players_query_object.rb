ad99c968e783ad468e3652d98ec7a016dfda5d76
SupDex <symeton@gmail.com>
2020-04-04 18:10:46 +0300

    change queries and spec for new logic

--- app/queries/players/top_players_query_object.rb
+++ app/queries/players/top_players_query_object.rb
@@ -5,7 +5,7 @@ module Players::TopPlayersQueryObject
     scope =
       Player
         .select('COUNT(indicator_id) AS cn_indicators, players.id')
-        .joins(:match_player_indicators)
+        .joins(match_player: :match_player_indicators)
         .where(match_player_indicators: { indicator_id: indicator.id })
         .group('indicator_id, players.id')
         .order('cn_indicators DESC')
--- spec/queries/players/top_players_query_object_spec.rb
+++ spec/queries/players/top_players_query_object_spec.rb
@@ -6,16 +6,34 @@ describe Players::TopPlayersQueryObject do
   let(:team_one) { create(:team) }
   let(:team_two) { create(:team) }
 
+  let(:match) { create(:match) }
+  let(:match_two) { create(:match) }
+  let(:match_three) { create(:match) }
+
   let(:player) { create(:player, team: team_one) }
   let(:player_two) { create(:player, team: team_two) }
   let(:player_three) { create(:player, team: team_two) }
+  let(:player_four) { create(:player, team: team_two) }
+
+  let(:match_player_one) { create(:match_player, player: player, match: match) }
+  let(:match_player_two) { create(:match_player, player: player_two, match: match) }
+  let(:match_player_three) { create(:match_player, player: player_three, match: match) }
 
   describe '#call' do
     before do
-      create_list(:match_player_indicator, 5, indicator: indicator)
-      create_list(:match_player_indicator, 4, player: player, indicator: indicator)
-      create_list(:match_player_indicator, 3, player: player_two, indicator: indicator)
-      create_list(:match_player_indicator, 2, player: player_three, indicator: indicator)
+      create_list(:match_player_indicator, 2, indicator: indicator)
+
+      4.times do
+        create(:match_player_indicator, match_player: create(:match_player, player: player), indicator: indicator)
+      end
+
+      3.times do
+        create(:match_player_indicator, match_player: create(:match_player, player: player_two), indicator: indicator)
+      end
+
+      2.times do
+        create(:match_player_indicator, match_player: create(:match_player, player: player_three), indicator: indicator)
+      end
     end
 
     it 'returns top 5 players by all teams' do
@@ -29,7 +47,7 @@ describe Players::TopPlayersQueryObject do
 
     it 'returns top 5 players by team' do
       3.times do
-        create(:match_player_indicator, player: create(:player, team: team_two), indicator: indicator)
+        create(:match_player_indicator, match_player: create(:match_player, player: create(:player, team: team_two)), indicator: indicator)
       end
 
       top_five_players = described_class.call(indicator: indicator, team: team_two)
